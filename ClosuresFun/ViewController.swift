3ea37b0ea319dc0c5415db38ece363f1fb58ba19
Stanislav <stazilastas@gmail.com>
2023-09-27 09:25:12 -0500

    Initial Commit

---
+++ ClosuresFun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClosuresFun
+//
+//  Created by STANISLAV STAJILA on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
